845226e25f9d36b6c106965f5fd89e2a81e064b0
Sultan Md. Alnur <mr.nur30@gmail.com>
2024-05-11 12:52:49 +0600

    add shimmerEffect

--- lib/widgets/shimmer_placeholder.dart
+++ lib/widgets/shimmer_placeholder.dart
@@ -7,9 +7,60 @@ Widget buildShimmerPlaceholder() {
   return Shimmer.fromColors(
     baseColor: matteBlack,
     highlightColor: Colors.deepOrangeAccent[700]!,
-    direction: ShimmerDirection.ttb,
+    direction: ShimmerDirection.ltr,
     child: Container(
       color: Colors.white,
     ),
   );
 }
+
+
+Widget buildShimmerEffect() {
+  return Shimmer.fromColors(
+    baseColor: Colors.grey[300]!,
+    highlightColor: Colors.grey[100]!,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 12),
+        Expanded(
+          flex: 1,
+          child: Container(
+            width: double.infinity,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 16),
+        Expanded(
+          flex: 3, // Set a fixed height for the GridView
+          child: GridView.builder(
+            primary: false,
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: 6, // Placeholder count
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                color: Colors.white,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Container(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+
+
+
