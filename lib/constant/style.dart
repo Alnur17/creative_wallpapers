33aef7313b444f86f9e84d2d81a8713cd6d3c2da
Alnur17 <mr.nur30@gmail.com>
2024-06-08 11:40:02 +0600

    added

---
+++ lib/constant/style.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'color_palate.dart';
+
+
+const styleRB24 = TextStyle(
+  color: textRed,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const styleRB20 = TextStyle(
+  color: textRed,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const styleWB24 = TextStyle(
+  color: textWhite,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const styleWB16 = TextStyle(
+  color: textWhite,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const styleWB20 = TextStyle(
+  color: textWhite,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
